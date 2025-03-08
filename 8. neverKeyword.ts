5d5b0fa6475e7fba5072449388586bfeac0becd5
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-03-08 08:53:54 +0530

    never keyword in TypeScript

---
+++ 8. neverKeyword.ts
@@ -0,0 +1,5 @@
+//The never keyword is used to indicate that a function will not return anything.
+//Or that a variable can never have a value.
+//The never type is useful for indicating that certain code paths should never
+//be reached, or that certain values are impossible. It can help catch errors at
+//compile-time instead of runtime.
